ab33f996b9f4af592b50fe2f4e74f7ae9bc186e1
Dave Landry <landry.dave@gmail.com>
2021-07-22 16:07:58 -0400

    updates populate script to work with subnational embeds

--- config.populate.js
+++ config.populate.js
@@ -5,12 +5,12 @@ const axios = require("axios"),
 const prefix = process.env.DOMAIN || "https://oec.world";
 
 const embeds = [
-  "/en/visualize/embed/tree_map/hs92/export/{{memberSlug}}/all/show/2019/", // product exports by country
-  // "/en/visualize/embed/tree_map/{{slug}}/export/{{memberSlug}}/all/show/2020/" // product exports by subnat
+  // "/en/visualize/embed/tree_map/hs92/export/{{memberSlug}}/all/show/2019/", // product exports by country
+  "/en/visualize/embed/tree_map/{{slug}}/export/{{id}}/all/show/2020/" // product exports by subnat
 ];
 
-const searchURL = "api/profilesearch?profile=1&limit=10000"; // countries
-// const searchURL = "api/profilesearch?profile=38&limit=10000"; // regional
+// const searchURL = "api/profilesearch?profile=1&limit=10000"; // countries
+const searchURL = "api/profilesearch?profile=38&limit=10000"; // regional
 
 const profiles = axios.get(new URL(searchURL, prefix).href)
   .then(resp => resp.data)
@@ -25,9 +25,9 @@ Promise.all([profiles])
         locations
           // .slice(0, 1)
           // .filter(d => ["twn"].includes(d.memberSlug))
-          .forEach(({memberSlug, slug}) => {
+          .forEach(({id, memberSlug, slug}) => {
             urls.push([
-              embed.replace("{{memberSlug}}", memberSlug).replace("{{slug}}", slug),
+              embed.replace("{{memberSlug}}", memberSlug).replace("{{slug}}", slug).replace("{{id}}", id),
               `https://oec.world/en/profile/${slug}/${memberSlug}`
             ]);
           });
