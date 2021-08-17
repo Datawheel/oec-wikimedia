4869b13ac9b5838856532edcc5c857df97b9ae47
Dave Landry <landry.dave@gmail.com>
2021-08-17 16:06:34 -0400

    deletes old files after successful upload

--- upload.js
+++ upload.js
@@ -18,41 +18,69 @@ function upload(files) {
   process.stdout.write(`\nUploading ${total - files.length} of ${total}`);
   const {title} = require(fileName.replace("svg", "js"));
 
+  function deleteFiles(fileName) {
+    fs.unlink(fileName, () => {});
+    fs.unlink(fileName.replace("svg", "js"), () => {});
+    fs.unlink(fileName.replace("svg", "txt"), () => {});
+  }
+
+  function next() {
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(title);
+
+    if (files.length) {
+      setTimeout(() => upload(files), 7500);
+    }
+    else {
+      console.log("\n\ndone!");
+      process.exit(0);
+    }
+  }
+
   fs.readFile(fileName, "utf8", (err, svg) => {
     if (err) {
       console.log("\nSVG readFile:");
       console.error(err);
+      next(title);
     }
-    fs.readFile(fileName.replace("svg", "txt"), "utf8", (err, txt) => {
-      if (err) {
-        console.log("\nTXT readFile:");
-        console.error(err);
-      }
-      client.upload(title, svg, "updates SVG", err => {
+    else {
+
+      fs.readFile(fileName.replace("svg", "txt"), "utf8", (err, txt) => {
         if (err) {
-          console.log("\nSVG upload:");
+          console.log("\nTXT readFile:");
           console.error(err);
+          next(title);
         }
-        client.edit(`File:${title}.svg`, txt, "updates description", err => {
-          if (err) {
-            console.log("\nTXT edit:");
-            console.error(err);
-          }
-
-          process.stdout.clearLine();
-          process.stdout.cursorTo(0);
-          process.stdout.write(title);
-
-          if (files.length) {
-            setTimeout(() => upload(files), 7500);
-          }
-          else {
-            console.log("\n\ndone!");
-            process.exit(0);
-          }
-        });
+        else {
+          client.upload(title, svg, "updates SVG", err => {
+            if (err && err.message.includes("duplicate")) {
+              deleteFiles(fileName);
+              next(title);
+            }
+            else if (err) {
+              console.log("\nSVG upload:");
+              console.error(err);
+              next(title);
+            }
+            else {
+              client.edit(`File:${title}.svg`, txt, "updates description", err => {
+                if (err) {
+                  console.log("\nTXT edit:");
+                  console.error(err);
+                }
+                else {
+                  deleteFiles(fileName);
+                }
+                next(title);
+              });
+            }
+          });
+        }
       });
-    });
+
+    }
   });
 
 }
